43bc427b4763f12184daa45eed16dc92b6897336
Pierre7Agus <redtor_90@outlook.es>
2023-09-11 17:59:51 -0500

    Add shift change functionality

--- 01-tic-tac-toe/App.jsx
+++ 01-tic-tac-toe/App.jsx
@@ -1,13 +1,18 @@
 /* eslint-disable space-before-function-paren */
+import { useState } from 'react'
 import './style.css'
 const TURNS = {
   X: 'x',
   O: 'o'
 }
 
-const Square = ({ children }) => {
+const Square = ({ children, isSelected, updateBoard }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+  const handleClick = () => {
+    updateBoard()
+  }
   return (
-    <div className='square'>
+    <div onClick={handleClick} className={className}>
       {children}
     </div>
   )
@@ -15,6 +20,13 @@ const Square = ({ children }) => {
 
 export function App() {
   const board = new Array(9).fill('a')
+  const [turn, setTurn] = useState(TURNS.X)
+
+  const updateBoard = () => {
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+  }
+
   return (
     <>
       <main className='board'>
@@ -23,13 +35,21 @@ export function App() {
           {
             board.map((_, index) => {
               return (
-                <Square key={index}>
+                <Square key={index} updateBoard={updateBoard}>
                   {board[index]}
                 </Square>
               )
             })
           }
         </div>
+        <section className='turn'>
+          <Square isSelected={TURNS.X === turn}>
+            {TURNS.X}
+          </Square>
+          <Square isSelected={TURNS.O === turn}>
+            {TURNS.O}
+          </Square>
+        </section>
       </main>
     </>
   )
