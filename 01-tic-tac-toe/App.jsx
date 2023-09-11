186f062837b989786c0685a7c5a9c330983e28d7
Pierre7Agus <redtor_90@outlook.es>
2023-09-11 17:28:46 -0500

    Create first component

--- 01-tic-tac-toe/App.jsx
+++ 01-tic-tac-toe/App.jsx
@@ -1,8 +1,29 @@
 /* eslint-disable space-before-function-paren */
+import './style.css'
+const TURNS = {
+  X: 'x',
+  O: 'o'
+}
+
 export function App() {
+  const board = new Array(9).fill('a')
   return (
     <>
-      <h1>Hello </h1>
+
+      <main className='board'>
+        <h1>Tic Tac Toe</h1>
+        <div className='game'>
+          {
+            board.map((_, index) => {
+              return (
+                <div className='square' key={index}>
+                  {board[index]}
+                </div>
+              )
+            })
+          }
+        </div>
+      </main>
     </>
   )
 }
