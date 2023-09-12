42462246e553d26b4d5d6922163f4aeb22526356
Pierre7Agus <redtor_90@outlook.es>
2023-09-12 12:29:57 -0500

    Add new functionality
    Added a way to know the winner, create an array with all the possible winning options, then inside a function use a for loop to do the iterations. A way was also created to check if the game has ended, this was done using the state and using the every method.

--- 01-tic-tac-toe/App.jsx
+++ 01-tic-tac-toe/App.jsx
@@ -6,6 +6,17 @@ const TURNS = {
   O: 'o'
 }
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
 const Square = ({ children, isSelected, updateBoard, index }) => {
   const className = `square ${isSelected ? 'is-selected' : ''}`
   const handleClick = () => {
@@ -21,12 +32,32 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
 export function App() {
   const [board, setBoard] = useState(new Array(9).fill(null))
   const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null) // sea false hay empate y cuando no sea ni false ni null hay ganador
+
+  const checkWinner = (newBoard) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (newBoard[a] && newBoard[a] === newBoard[b] && newBoard[a] === newBoard[c]) {
+        return newBoard[a]
+      }
+    }
+  }
+
+  const checkEndGame = (newBoard) => {
+    return newBoard.every(square => square !== null)
+  }
 
   const updateBoard = (index) => {
-    if (board[index]) return
+    if (board[index] || winner) return
     const newBoard = structuredClone(board)
     newBoard[index] = turn
     setBoard(newBoard)
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false)
+    }
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
   }
@@ -55,6 +86,25 @@ export function App() {
           </Square>
         </section>
       </main>
+
+      {
+
+        winner !== null && (
+          <section className='winner'>
+            <div className='text'>
+              <h2>{winner ? 'Ganó: ' : 'Empate'}</h2>
+              <header className='win'>
+                {winner && <Square>{winner}</Square>}
+              </header>
+              <footer>
+                <button>Empezar de nuevo</button>
+              </footer>
+            </div>
+          </section>
+        )
+
+      }
+
     </>
   )
 }
