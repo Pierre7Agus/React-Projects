7b531a5aa52ab958ea743f369a51a840c68bb74e
Pierre7Agus <redtor_90@outlook.es>
2023-09-15 21:00:55 -0500

    Fix error in the config in second project

--- 02-mouse-follower/src/main.jsx
+++ 02-mouse-follower/src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import { React } from 'react/client'
+import React from 'react'
 import App from './App'
 
 const root = createRoot(document.getElementById('app'))
