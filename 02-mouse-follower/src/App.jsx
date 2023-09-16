bdedb14bad267f943c3d966b713ba68ffc071b80
Pierre7Agus <redtor_90@outlook.es>
2023-09-15 22:17:57 -0500

    Fix position

--- 02-mouse-follower/src/App.jsx
+++ 02-mouse-follower/src/App.jsx
@@ -1,17 +1,19 @@
-import { useEffect, useState } from 'react'
+import { useState, useEffect } from 'react'
 export default function App() {
   const [enabled, setEnabled] = useState(false)
-  const [position, setPosition] = useState({ x: 0, y: 0 })
+  const [position, setPosition] = useState({ x: -100, y: -100 })
 
   useEffect(() => {
     const handleMove = (event) => {
       const { clientX, clientY } = event
       setPosition({ x: clientX, y: clientY })
     }
-
     if (enabled) {
       window.addEventListener('pointermove', handleMove)
+    } else if (!enabled) {
+      setPosition({ x: -100, y: -100 })
     }
+
     return () => {
       window.removeEventListener('pointermove', handleMove)
     }
@@ -20,21 +22,21 @@ export default function App() {
   return (
     <main className='contenedor'>
       <div
-        className='follower'
         style={{
           position: 'absolute',
-          background: 'orange',
+          zIndex: '-1',
           width: '40px',
           height: '40px',
-          top: '-20px',
+          background: 'orange',
           pointerEvents: 'none',
-          left: '-20px',
+          top: '-25px',
+          left: '-25px',
           borderRadius: '50%',
-          transform: `translate(${position.x}px, ${position.y}px)`
+          transform: `translate(${position.x}px,${position.y}px)`
         }}
       />
       <h1>Mouse Follower</h1>
-      <button onClick={() => setEnabled(!enabled)}> {enabled ? 'Desactivar' : 'Activar'} Mouse Follower </button>
+      <button onClick={() => setEnabled(!enabled)}>{enabled ? 'Desactivar' : 'Activar'} Mouse Follower</button>
     </main>
   )
 }
