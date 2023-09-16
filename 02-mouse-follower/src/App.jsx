d127ff9fdde6adcfac707a3ffa8a22cdc9f4241a
Pierre7Agus <redtor_90@outlook.es>
2023-09-15 22:08:19 -0500

    Add pointer follower

--- 02-mouse-follower/src/App.jsx
+++ 02-mouse-follower/src/App.jsx
@@ -1,7 +1,40 @@
+import { useEffect, useState } from 'react'
 export default function App() {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
   return (
-    <>
-      <h1>Hola mi gente</h1>
-    </>
+    <main className='contenedor'>
+      <div
+        className='follower'
+        style={{
+          position: 'absolute',
+          background: 'orange',
+          width: '40px',
+          height: '40px',
+          top: '-20px',
+          pointerEvents: 'none',
+          left: '-20px',
+          borderRadius: '50%',
+          transform: `translate(${position.x}px, ${position.y}px)`
+        }}
+      />
+      <h1>Mouse Follower</h1>
+      <button onClick={() => setEnabled(!enabled)}> {enabled ? 'Desactivar' : 'Activar'} Mouse Follower </button>
+    </main>
   )
 }
